dde4fa75df7072e906443c512e2eaa6c1bb010a7
Gyuwon Yi <gyuwon@live.com>
2024-12-17 11:18:22 +0900

    Update AutoParams to 9.0.0

--- easymapper/src/test/java/test/easymapper/AutoParameterizedTest.java
+++ easymapper/src/test/java/test/easymapper/AutoParameterizedTest.java
@@ -2,13 +2,13 @@ package test.easymapper;
 
 import autoparams.AutoSource;
 import autoparams.customization.Customization;
-import autoparams.customization.SettablePropertyWriter;
 import autoparams.mockito.MockitoCustomizer;
-import org.junit.jupiter.params.ParameterizedTest;
+import autoparams.processor.InstancePropertyWriter;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.params.ParameterizedTest;
 
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
@@ -16,7 +16,7 @@ import java.lang.annotation.Target;
 @AutoSource
 @Customization({
     MockitoCustomizer.class,
-    SettablePropertyWriter.class
+    InstancePropertyWriter.class
 })
 public @interface AutoParameterizedTest {
 }
