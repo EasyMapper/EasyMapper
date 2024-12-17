598156e258408cceeb61ea5dc40eb23c5fc9d22d
Gyuwon Yi <gyuwon@live.com>
2024-12-18 02:48:50 +0900

    Refactor Tuple with Lombok

--- easymapper/src/main/java/easymapper/Tuple.java
+++ easymapper/src/main/java/easymapper/Tuple.java
@@ -2,13 +2,12 @@ package easymapper;
 
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 class Tuple {
 
     private final Map<String, Object> properties;
-
-    public Tuple(Map<String, Object> properties) {
-        this.properties = properties;
-    }
 
     public Object get(String name) {
         return properties.get(name);
