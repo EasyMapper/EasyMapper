2a222739e181aa8303ee2a935ad56df5dda50fe5
Gyuwon Yi <gyuwon@live.com>
2024-12-18 02:52:46 +0900

    Refactor TypeReference<T> with Lombok

--- easymapper/src/main/java/easymapper/TypeReference.java
+++ easymapper/src/main/java/easymapper/TypeReference.java
@@ -3,6 +3,9 @@ package easymapper;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import lombok.Getter;
+
+@Getter
 @SuppressWarnings("unused")
 public abstract class TypeReference<T> {
 
@@ -11,9 +14,5 @@ public abstract class TypeReference<T> {
     protected TypeReference() {
         ParameterizedType reference = (ParameterizedType) getClass().getGenericSuperclass();
         this.type = reference.getActualTypeArguments()[0];
-    }
-
-    public Type getType() {
-        return type;
     }
 }
