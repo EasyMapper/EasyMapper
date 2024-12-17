e9de25df874ce95f201b311a76a4f1207b621fcc
Gyuwon Yi <gyuwon@live.com>
2024-12-18 02:50:56 +0900

    Refactor TypeAnalyzer

--- easymapper/src/main/java/easymapper/TypeAnalyzer.java
+++ easymapper/src/main/java/easymapper/TypeAnalyzer.java
@@ -18,25 +18,31 @@ class TypeAnalyzer {
         } else if (type instanceof ParameterizedType) {
             return getReturnTypeResolver((ParameterizedType) type);
         } else {
-            String message = "Cannot support return type resolver for the type: " + type;
-            throw new RuntimeException(message);
+            throw new RuntimeException(
+                "Cannot support return type resolver for the type: " + type
+            );
         }
     }
 
-    private static Function<Getter, Type> getReturnTypeResolver(ParameterizedType type) {
+    private static Function<Getter, Type> getReturnTypeResolver(
+        ParameterizedType type
+    ) {
         Map<TypeVariable<?>, Type> typeVariableMap = getTypeVariableMap(type);
         return getter -> getter.type() instanceof TypeVariable<?>
             ? typeVariableMap.get((TypeVariable<?>) getter.type())
             : getter.type();
     }
 
-    private static Map<TypeVariable<?>, Type> getTypeVariableMap(ParameterizedType type) {
+    private static Map<TypeVariable<?>, Type> getTypeVariableMap(
+        ParameterizedType type
+    ) {
         TypeVariable<?>[] typeParameters = getTypeParameters(type);
         Type[] actualTypeArguments = type.getActualTypeArguments();
         Map<TypeVariable<?>, Type> typeVariableMap = new HashMap<>();
         for (int i = 0; i < typeParameters.length; i++) {
             typeVariableMap.put(typeParameters[i], actualTypeArguments[i]);
         }
+
         return Collections.unmodifiableMap(typeVariableMap);
     }
 
@@ -44,8 +50,9 @@ class TypeAnalyzer {
         if (type.getRawType() instanceof Class<?>) {
             return ((Class<?>) type.getRawType()).getTypeParameters();
         } else {
-            String message = "Cannot provide type parameters for the type: " + type;
-            throw new RuntimeException(message);
+            throw new RuntimeException(
+                "Cannot provide type parameters for the type: " + type
+            );
         }
     }
 }
