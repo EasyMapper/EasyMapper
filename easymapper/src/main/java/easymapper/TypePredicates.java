489d6a1dcfdccc79ce8119194083dbde22854d2c
Gyuwon Yi <gyuwon@live.com>
2024-12-18 02:51:26 +0900

    Delete TypePredicates

--- easymapper/src/main/java/easymapper/TypePredicates.java
+++
@@ -1,9 +0,0 @@
-package easymapper;
-
-import java.lang.reflect.Type;
-import java.util.function.Function;
-
-class TypePredicates {
-
-    public static Function<Type, Boolean> acceptAllTypes = type -> true;
-}
