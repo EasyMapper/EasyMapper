a73d154305bec0c46943973d3286828e12f022ce
Gyuwon Yi <gyuwon@live.com>
2024-12-18 11:56:37 +0900

    Refactor tests for inheritance

--- easymapper/src/test/java/test/easymapper/Mapper_specs.java
+++ easymapper/src/test/java/test/easymapper/Mapper_specs.java
@@ -1352,4 +1352,62 @@ class Mapper_specs {
         assertThat(destination.getShipment().getAddress().getZipCode())
             .isEqualTo(source.getShipmentAddressZipCode());
     }
+
+    @Getter
+    public static class Employee extends User {
+
+        private final long departmentId;
+
+        @ConstructorProperties({
+            "id",
+            "username",
+            "passwordHash",
+            "departmentId"
+        })
+        public Employee(
+            long id,
+            String username,
+            String passwordHash,
+            long departmentId
+        ) {
+            super(id, username, passwordHash);
+            this.departmentId = departmentId;
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class EmployeeView extends UserView {
+
+        private long departmentId;
+    }
+
+    @AutoParameterizedTest
+    void convert_correctly_sets_inherited_properties_through_setters(
+        Mapper sut,
+        Employee source
+    ) {
+        EmployeeView actual = sut.convert(source, EmployeeView.class);
+        assertThat(actual.getUsername()).isEqualTo(source.getUsername());
+    }
+
+    @AutoParameterizedTest
+    void convert_correctly_sets_inherited_properties_through_constructors(
+        EmployeeView view
+    ) {
+        Mapper sut = new Mapper(
+            config -> config.map(
+                EmployeeView.class,
+                Employee.class,
+                mapping -> mapping.compute(
+                    "passwordHash",
+                    (source, context) -> null
+                )
+            )
+        );
+
+        Employee actual = sut.convert(view, Employee.class);
+
+        assertThat(actual.getUsername()).isEqualTo(view.getUsername());
+    }
 }
